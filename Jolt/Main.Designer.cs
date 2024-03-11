247124f98f4de9426a4ad998d128587c45a97896
Robert Domico <robert.domico@live.com>
2024-03-10 20:52:00 -0400

    Add project files.

---
+++ Jolt/Main.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Jolt
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnHide = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(109, 67);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(75, 23);
+            this.btnHide.TabIndex = 0;
+            this.btnHide.Text = "Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.BtnHide_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Interval in Seconds";
+            // 
+            // maskedTextBox
+            // 
+            this.maskedTextBox.Location = new System.Drawing.Point(23, 31);
+            this.maskedTextBox.Mask = "00000";
+            this.maskedTextBox.Name = "maskedTextBox";
+            this.maskedTextBox.Size = new System.Drawing.Size(41, 20);
+            this.maskedTextBox.TabIndex = 3;
+            this.maskedTextBox.ValidatingType = typeof(int);
+            // 
+            // Main
+            // 
+            this.AcceptButton = this.btnHide;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(195, 101);
+            this.ControlBox = false;
+            this.Controls.Add(this.maskedTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHide);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(211, 140);
+            this.MinimumSize = new System.Drawing.Size(211, 140);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Jolt";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox;
+    }
+}
+
---
+++ Jolt/Main.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Jolt
+{
+    public partial class Main : Form
+    {
+        private readonly NotifyIcon sysTrayIcon;
+        private readonly ContextMenu sysTrayMenu;
+        private readonly Timer interval = new Timer();
+        private int seconds = 300;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            sysTrayMenu = new ContextMenu();
+            sysTrayMenu.MenuItems.Add("Enable Jolt", OnEnabled);
+            sysTrayMenu.MenuItems.Add("Disable Jolt", OnDisabled);
+            sysTrayMenu.MenuItems.Add("Edit Interval", OnShowed);
+            sysTrayMenu.MenuItems.Add("Exit", OnExit);
+
+            sysTrayIcon = new NotifyIcon
+            {
+                Text = "Jolt",
+                Icon = new Icon(SystemIcons.Asterisk, 40, 40),
+                ContextMenu = sysTrayMenu,
+                Visible = true
+            };
+
+            StartPosition = FormStartPosition.Manual;
+            Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - Width,
+                                   Screen.PrimaryScreen.WorkingArea.Height - Height);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+
+            maskedTextBox.Text = seconds.ToString();
+
+            interval.Interval = seconds * 1000;
+            interval.Tick += new EventHandler(SendKey);
+
+            sysTrayMenu.MenuItems[0].Checked = true;
+            interval.Start();
+
+            base.OnLoad(e);
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void OnEnabled(object sender, EventArgs e)
+        {
+            interval.Start();
+            sysTrayMenu.MenuItems[0].Checked = true;
+            sysTrayMenu.MenuItems[1].Checked = false;
+        }
+        private void OnDisabled(object sender, EventArgs e)
+        {
+            interval.Stop();
+            sysTrayMenu.MenuItems[0].Checked = false;
+            sysTrayMenu.MenuItems[1].Checked = true;
+        }
+        private void OnShowed(object sender, EventArgs e)
+        {
+            Visible = true;
+        }
+        private void SendKey(object sender, EventArgs e)
+        {           
+            SendKeys.Send("{F15}");
+        }       
+
+        private void BtnHide_Click(object sender, EventArgs e)
+        {
+            seconds = Convert.ToInt32(maskedTextBox.Text);
+            interval.Interval = seconds * 1000;
+            Visible = false;
+        }
+    }
+}
